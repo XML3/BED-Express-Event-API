ea328327f4064916189d0c750b7190711a8fc6d7
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-11 20:10:29 +0100

    created Router for imgAnimation by ID

---
+++ starter/routes/imgAnimation.js
@@ -0,0 +1,25 @@
+import express from "express";
+import getImgAnimationById from "../src/services/animation/getImgAnimation";
+
+const router = express.Router();
+
+//GET imgAnimation by ID
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const animation = await getImgAnimationById(id);
+
+    if (!animation) {
+      res.status(404).send({
+        message: `Image Animation with id ${id} was not found`,
+        animation,
+      });
+    } else {
+      res.status(200).json(animation);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
