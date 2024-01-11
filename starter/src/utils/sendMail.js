ce2a6f54401ee202e9aa7f22952d58588df63f01
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-11 01:10:58 +0100

    created new Util for oAuth2, nodemailer. added sendMail function

---
+++ starter/src/utils/sendMail.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
+
+const sendMail = async (mailOptions) => {
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+export default transporter;
