343e6fed778dddf137a986ffc77bc78bf2491512
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-17 14:18:20 +0100

    added comments

--- starter/src/middleware/uploadMiddleware.js
+++ starter/src/middleware/uploadMiddleware.js
@@ -3,3 +3,6 @@ import multer from "multer";
 const upload = multer({ dest: "uploads/" });
 
 export default upload;
+
+//This Middleware is set to work in the Routers of Events Create and Update.
+//It it related to the uploading of user's image from a device on to a hosting photo site
