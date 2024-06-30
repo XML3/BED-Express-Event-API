5f86685a339a068d72bb2f14b2333d5165006567
Xagly Montilva <xaglymontilva@gmail.com>
2024-06-30 15:02:51 +0200

    modified to integrate ReCaptcha verification logic

--- starter/routes/contact.js
+++ starter/routes/contact.js
@@ -1,11 +1,18 @@
 import express from "express";
+import createContactEntry from "../src/services/contact/contact.js";
+import verifyRecaptcha from "../src/utils/verifyRecaptcha.js";
 
 const router = express.Router();
 
 //POST = Create Contact
 router.post("/", async (req, res, next) => {
   try {
-    const { name, email, message } = req.body;
+    const { name, email, message, recaptchaToken } = req.body;
+
+    const isRecaptchaValid = await verifyRecaptcha(recaptchaToken);
+    if (!isRecaptchaValid) {
+      return res.status(400).json({ error: "Invalid reCAPTCHA token" });
+    }
 
     const newContact = await createContactEntry(name, email, message);
     res.status(201).json(newContact);
