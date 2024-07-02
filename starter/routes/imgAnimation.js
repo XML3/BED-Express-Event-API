7a6fa919480e4f0deb95d28dff97496b16965bd6
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-02 12:10:15 +0200

    added missing `js` inside import section

--- starter/routes/imgAnimation.js
+++ starter/routes/imgAnimation.js
@@ -1,5 +1,5 @@
 import express from "express";
-import getImgAnimationById from "../src/services/animation/getImgAnimation";
+import getImgAnimationById from "../src/services/animation/getImgAnimation.js";
 
 const router = express.Router();
 
