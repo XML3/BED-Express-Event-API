545db2ea6aa828aada38dd4a03d3c804cc2eb226
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-03 20:00:56 +0200

    added a new route to fetch all imgAnimation

--- starter/routes/imgAnimation.js
+++ starter/routes/imgAnimation.js
@@ -1,7 +1,18 @@
 import express from "express";
 import getImgAnimationById from "../src/services/animation/getImgAnimation.js";
+import getImgAnimation from "../src/services/animation/getImgAnimationAll.js";
 
 const router = express.Router();
+
+//Fetch All
+router.get("/", async (req, res, next) => {
+  try {
+    const animation = await getImgAnimation();
+    res.status(200).json(animation);
+  } catch (error) {
+    next(error);
+  }
+});
 
 //GET imgAnimation by ID
 router.get("/:id", async (req, res, next) => {
