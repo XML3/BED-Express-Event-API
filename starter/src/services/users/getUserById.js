82ffdf4f6b0bb3a1ce8bf448b090e5c930602563
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-08 22:13:45 +0200

    back

--- starter/src/services/users/getUserById.js
+++ starter/src/services/users/getUserById.js
@@ -3,13 +3,6 @@ import prisma from "../../../lib/prismaClient.js";
 const getUserById = async (id) => {
   const user = await prisma.user.findUnique({
     where: { id },
-    include: {
-      select: {
-        id: true,
-        name: true,
-        image: true,
-      },
-    },
   });
 
   return user;
