bc307305d2258fbcb197f965665100743a03f755
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-12 18:51:38 +0200

    removed upload logic

--- starter/src/services/events/createEvent.js
+++ starter/src/services/events/createEvent.js
@@ -1,5 +1,5 @@
 import prisma from "../../../lib/prismaClient.js";
-import uploadFileToImgBB from "../../utils/fileUpload.js";
+// import uploadFileToImgBB from "../../utils/fileUpload.js";
 
 const createEvent = async (
   title,
@@ -8,20 +8,19 @@ const createEvent = async (
   image,
   startTime,
   endTime,
+  lineup,
   createdBy,
   categoryIds
 ) => {
-  //upload the image and get the ImgBB URL
-  const imageUrl = image ? await uploadFileToImgBB(image.path) : null;
-
   const event = await prisma.event.create({
     data: {
       title,
       description,
       location,
-      image: imageUrl,
+      image,
       startTime,
       endTime,
+      lineup,
       createdBy: {
         connect: { id: createdBy },
       },
